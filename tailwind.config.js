ef5753cf024f8b2451f58bbbecca737a7bf12315
Amalkrishnanvp <Amalyedhu10@gmail.com>
2024-08-25 08:48:18 +0530

    Completed Footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,14 @@ module.exports = {
       colors: {
         "custom-color-1": "#0d1b2a",
         "custom-color-2": "#303942",
+        "custom-color-3": "#d1d5db",
+        "custom-color-4": "#9ca3af",
+      },
+      fontSize: {
+        "custom-font-12": "12px",
+      },
+      borderWidth: {
+        "custom-thin-1": "1px",
       },
     },
   },
