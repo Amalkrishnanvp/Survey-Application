13959f4c0ee3aaf25614afb192d9d1cf2d222630
Amalkrishnanvp <Amalyedhu10@gmail.com>
2024-08-27 16:57:25 +0530

    Connected to MongoDB database

--- routes/register.js
+++ routes/register.js
@@ -6,18 +6,36 @@ const { MongoClient } = require("mongodb");
 /* GET - Render Registration page */
 router.get("/", (req, res) => {
   res.render("register");
-
-  async function name(params) {
-        
-    const uri = "mongodb://localhost:27017";
-
-  }
 });
 
 /* POST - Handle Registration logic */
 router.post("/", (req, res) => {
   res.send("registration success");
   console.log(req.body);
+
+  async function main() {
+    const uri = "mongodb://localhost:27017";
+    const client = new MongoClient(uri);
+
+    const dbName = "amal";
+
+    try {
+      await client.connect();
+      console.log("Connected to MongoDB");
+
+      const database = client.db(dbName);
+      const collection = database.collection("user");
+
+      const first = await collection.findOne();
+      console.log(first);
+    } catch (error) {
+      console.log("Some error occured: ", error);
+    } finally {
+      await client.close();
+    }
+  }
+
+  main().catch(console.error);
 });
 
 module.exports = router;
