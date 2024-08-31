df08762ae7201956641fd1f9a1510b49cdc395e0
Amalkrishnanvp <Amalyedhu10@gmail.com>
2024-08-31 20:21:29 +0530

    Added password toggling feature

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuBtn = document.querySelector(".menu-btn");
   const mobileMenu = document.querySelector("#mobile-menu");
+  const passwordEyeIcon = document.querySelector(".password-eye-icon");
+  const confirmPasswordEyeIcon = document.querySelector(
+    ".confirm-password-eye-icon"
+  );
+  const passwordInput = document.querySelector("#password");
+  const confirmPasswordInput = document.querySelector("#confirmPassword");
 
   menuBtn.addEventListener("click", () => {
     mobileMenu.classList.toggle("hidden");
   });
+
+  passwordEyeIcon.addEventListener("click", () => {
+    togglePassword();
+  });
+
+  confirmPasswordEyeIcon.addEventListener("click", () => {
+    toggleConfirmPassword();
+  });
+
+  function togglePassword() {
+    if (passwordInput.value) {
+      const passwordType = passwordInput.getAttribute("type");
+
+      if (passwordType === "password") {
+        passwordInput.setAttribute("type", "text");
+      } else {
+        passwordInput.setAttribute("type", "password");
+      }
+    }
+  }
+
+  function toggleConfirmPassword() {
+    if (confirmPasswordInput.value) {
+      const passwordType = confirmPasswordInput.getAttribute("type");
+
+      if (passwordType === "password") {
+        confirmPasswordInput.setAttribute("type", "text");
+      } else {
+        confirmPasswordInput.setAttribute("type", "password");
+      }
+    }
+  }
 });
